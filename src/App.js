98be9e2a50ca7f2dbcbe29b268ff704d1558190e
Hansjason97 <hansadiogo@gmail.com>
2023-05-23 01:55:07 +0100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Home from "./components/Home";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import Blog from "./components/Blog";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/réalisations" element={<Portfolio />} />
+        <Route path="/articles" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
     </>
   );
 }
